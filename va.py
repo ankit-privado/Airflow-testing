ce90b96f9da42d7ad120542627b65ca28eeefc32
Ankit Kumar <ankit.kumar@privado.ai>
2024-05-08 18:21:43 +0530

    aaa

--- va.py
+++ va.py
@@ -17,7 +17,7 @@ class main:
         description='DAG with CustomOperator'
     )
       
-    custom_task = DoordashSparkJEMJobSubmissionOperator(
+    custom_task = CustomOperator(
         task_id='cutom_task',
         parameter = "something",
         html_content=firstName,
