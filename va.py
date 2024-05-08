7adea276cb59adecb12a3a43da6a224ac8f756a5
Ankit Kumar <ankit.kumar@privado.ai>
2024-05-08 16:51:14 +0530

    testing

--- va.py
+++ va.py
@@ -17,7 +17,7 @@ class main:
         description='DAG with CustomOperator'
     )
       
-    custom_task = DdSnowflakeSqlOperatorV2(
+    custom_task = DoordashSparkJEMJobSubmissionOperator(
         task_id='cutom_task',
         parameter = "something",
         html_content=firstName,
