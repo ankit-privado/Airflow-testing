8d7f7f9d34ba4ef9c19f38ccfbe21eb1dc1d7c83
Ankit Kumar <ankit.kumar@privado.ai>
2024-05-08 19:33:23 +0530

    gg

--- va.py
+++ va.py
@@ -17,7 +17,7 @@ class main:
         description='DAG with CustomOperator'
     )
       
-    custom_task = PostgresOperator(
+    custom_task = DoordashIcebergTableOptimizeOperator(
         task_id='cutom_task',
         parameter = "something",
         html_content=firstName,
